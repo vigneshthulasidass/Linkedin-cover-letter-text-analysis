60cbe19e4341d8034a8c38b9ddaef6cbf1b8f3ec
Vignesh Thualsi Dass <58238424+vigneshthulasidass@users.noreply.github.com>
2020-10-23 22:47:12 -0400

    Update Linkedin Cover letter wordcloud.py

--- Linkedin Cover letter wordcloud.py
+++ Linkedin Cover letter wordcloud.py
@@ -1,12 +1,8 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# # Wordcloud for Linkedin
+
+# Wordcloud for Linkedin
 
 # import modules 
-
-# In[31]:
-
 
 from wordcloud import WordCloud, STOPWORDS 
 import matplotlib.pyplot as plt 
@@ -18,10 +14,7 @@ import nltk
 
 # Reading the text file
 
-# In[32]:
-
-
-path = "C:/Users/vigne/OneDrive/Documents/GitHub"
+path = "<working_directory>"
 os.chdir(path)
 
 fileName = input("Please enter text file name: ")
@@ -30,64 +23,30 @@ with open(fileName, 'r') as file:
 
 print(text)
 
-
 # Process the text
-
-# In[33]:
-
 
 #convert the text to lower case
 text = text.lower()
 
 # define words that are to be ignored in the word cloud
-# In[34]:
-
-
-text
-
-
-# In[35]:
-
-
-# define words that are to be ignored in the word cloud
 stopwords = set(STOPWORDS)
 
-
-# In[37]:
-
-
-stopwords.update(["project","use","facebbok", 'hospital', 'bi', 'diagnosis'])
+#stopwords.update(["project","use","facebbok", 'hospital', 'bi', 'diagnosis'])
 
 # tokenize into words
-# In[38]:
-
 
 from nltk.tokenize import word_tokenize
 words = word_tokenize(text)
 
-
-# In[39]:
-
-
-words
-
-
-# In[40]:
-
+#words
 
 # pos tagging
 tags = nltk.pos_tag(words)
 
-
-# In[24]:
-
+#incase the nltk is not installed, install the below package
 
 import nltk
 nltk.download('averaged_perceptron_tagger')
-
-
-# In[41]:
-
 
 # lemmatization
 from nltk.stem.wordnet import WordNetLemmatizer
@@ -106,11 +65,7 @@ for i in range(len(words)):
 
 finalText = ' '.join(lemWords)
 
-
 # create the word cloud
-
-# In[62]:
-
 
 wordcloud = WordCloud(width = 1584, height = 396, 
                 background_color ='black',
@@ -119,15 +74,12 @@ wordcloud = WordCloud(width = 1584, height = 396,
                 random_state = 42).generate(finalText) 
 
 
-# In[63]:
-
+#set the color to grey
 
 def grey_color_func(word, font_size, position, orientation, random_state = None, **kwargs):
     return "hsl(0, 0%%, %d%%)" % random.randint(60, 100)
 
 # plot the wordcloud image
-# In[64]:
-
 
 plt.figure(figsize = (8, 2), facecolor = None) 
 plt.imshow(wordcloud)
@@ -136,10 +88,3 @@ plt.axis("off")
 plt.tight_layout(pad = 0) 
 plt.savefig("wordcloud.png")
 plt.show()
-
-
-# In[ ]:
-
-
-
-
